e47ac10001f3ad81f5143ea76a9d9da582534e20
Anderson290 <anderson.nunes@fcamara.com.br>
2019-03-18 21:18:18 -0300

    Post Watson Concluido

--- api/app.js
+++ api/app.js
@@ -1,8 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 const watson = require('watson-developer-cloud/assistant/v1');
+
+
+const app = express();
 
 const ASSISTANT_IAM_URL = 'https://gateway.watsonplatform.net/assistant/api';
 
 const ASSISTANT_IAM_APIKEY = '';
+
+app.use(bodyParser.json());
+
+const port = 3000;
+
+
 
 var chatbot = new watson({
     'username':'d2b20928-8536-4d46-9f4a-86c0cf2dcf7f',
@@ -12,23 +23,39 @@ var chatbot = new watson({
     'iam_url': 'https://iam.bluemix.net/identity/token'
 
 });
-
-
-
-
 var payload = {
     workspace_id: 'e7a92d12-195e-475d-a8e4-56e9ad5ee1c5',
     context: {},
     input: {}
 };
 
-
 chatbot.message(payload, function trataResposta(err, resposta){
     if(err){
         console.log(err)
     }
 
+
     if(resposta.output.text.length > 0){
         console.log(resposta);
     }
 });
+app.post('/conversation/', (req, res) => {
+    let text = req.body.message
+
+
+   var payload2 ={
+    workspace_id: 'e7a92d12-195e-475d-a8e4-56e9ad5ee1c5',
+    context: {},
+    input: {text}
+
+   }
+
+    chatbot.message(payload2, (err, resposta) => {
+
+        console.log(resposta)
+    
+    });
+})
+
+app.listen(port, () => console.log(`Running on port ${port}`));
+
