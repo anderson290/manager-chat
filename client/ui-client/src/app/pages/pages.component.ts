9680fb1119d968b2a72169ffc5dc736b9c4a7729
Anderson290 <anderson.nunes@fcamara.com.br>
2019-05-19 21:26:42 -0300

    Ajuste Scroll Chat - Uppercase - Age limit 100

--- client/ui-client/src/app/pages/pages.component.ts
+++ client/ui-client/src/app/pages/pages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { UserModel } from 'src/models/user';
 import { MessageService } from 'src/services/message.service';
 import { ConversationModel } from 'src/models/conversation';
@@ -10,7 +10,7 @@ import { ConversationModel } from 'src/models/conversation';
 })
 export class PagesComponent implements OnInit {
 
-
+  @ViewChild('divToScroll') divToScroll: ElementRef;
   bgPicture: any = 'assets/images/header.png';
   bgPictureComa: any = 'assets/images/backComa.png';
   userModel: UserModel = new UserModel();
@@ -86,14 +86,67 @@ export class PagesComponent implements OnInit {
     }
   }
 
+  scroll(el: HTMLElement) {
+    console.log(el)
+    // el.scrollLeft = 500;
+    el.scrollTop = 1000;
+  }
+
   async sendUser() {
 
     if (sessionStorage.getItem('contextId') != '') {
-      console.log("Segundo")
+      
 
       this.req = JSON.parse(sessionStorage.getItem('contextId'));
-      this.req.message = this.message;
-      this.responseMessageArr.push(this.message);
+
+      console.log(this.req);
+
+      if(this.req.output.text == "Qual sua idade?"){
+        if(parseInt(this.message) > 100){
+         alert("Idade ultrapassa nossos recursos"); 
+        }else{
+          this.req.message = this.message;      
+
+          this.responseMessageArr.push(this.message.charAt(0).toUpperCase() + this.message.slice(1));
+
+
+          this.responseMessage = await this.uibotService.sendMessage(this.req);
+          sessionStorage.setItem('contextId', JSON.stringify(this.responseMessage));
+    
+          if (this.responseMessage.output.generic[0].options) {
+            this.responseMessageArr.push(this.responseMessage.output.generic[0].title);
+            this.options = this.responseMessage.output.generic[0].options;
+            this.opRes = true;
+          } else {
+            this.responseMessageArr.push(this.responseMessage.output.text);
+            this.opRes = false;
+          }
+    
+          this.userModel.name = this.responseMessage.context.nome_user;
+          this.userModel.age = this.responseMessage.context.age_user;
+          this.userModel.location = this.responseMessage.context.location;
+          this.userModel.maritalStatus = this.responseMessage.context.maritalStatus;
+          this.userModel.sex = this.responseMessage.context.genero;
+          this.userModel.conversation = this.responseMessage;
+    
+          this.userArr = await this.uibotService.getUsers();
+    
+          for (let i of this.userArr) {
+            if (i.conversation[0].context.conversation_id == this.responseMessage.context.conversation_id) {
+              this.log = {
+                input: this.message,
+                output: this.responseMessage.output.text
+              }
+              this.userModel.conversation['output'].log_messages.push(this.log);
+              await this.uibotService.updateUser(i._id, this.userModel);
+            }
+          }
+        }
+      }else{
+      this.req.message = this.message;      
+
+        this.responseMessageArr.push(this.message.charAt(0).toUpperCase() + this.message.slice(1));
+        
 
       this.responseMessage = await this.uibotService.sendMessage(this.req);
       sessionStorage.setItem('contextId', JSON.stringify(this.responseMessage));
@@ -126,6 +179,9 @@ export class PagesComponent implements OnInit {
           await this.uibotService.updateUser(i._id, this.userModel);
         }
       }
+      }
+
+
 
     } else {
       console.log("Primeiro")
@@ -161,54 +217,8 @@ export class PagesComponent implements OnInit {
       this.userId = await this.uibotService.createConversation(this.userModel);
       console.log("ID", this.userId)
       //  sessionStorage.setItem('userId', id._id);
+
     }
-
-
-
-    //   this.conversationUser = response;
-
-
-    //   this.userModel.conversation[0] = this.conversationUser;
-
-    //   await this.uibotService.updateUser(this.userModel);
-
-    // this.users = await this.uibotService.getUsers();
-    // this.cont++;
-
-
-    // if(this.users != []){
-    //   for (let x of this.users) {
-
-    //     if (this.user._id == x._id) {
-    //       await this.uibotService.updateUser(this.user);
-    //       console.log('UPDATE', x);
-    //       this.req = {
-    //         "message": this.message,
-    //         "context": this.userModel.conversation[0].context
-    //       }
-
-    //       console.log("REQUISIÇÃO", this.req)
-    //     } else {
-    //       this.user = await this.uibotService.createConversation(this.userModel);
-    //       console.log('CRIOU')
-    //       this.req = {
-    //         "message": this.message,
-    //         "context": {}
-    //       }
-
-    //     }
-
-
-    //   }
-    // }else{
-    //   this.user = await this.uibotService.createConversation(this.userModel);
-    //       console.log('CRIOU')
-    //   this.req = {
-    //     "message": this.message,
-    //     "context": {}
-    //   }
-    // }
-
 
 
 
