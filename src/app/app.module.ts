71adfeff4ec27b7c589990615f5abf9db57017ed
Anderson290 <anderson.nunes@fcamara.com.br>
2019-09-14 00:50:56 -0300

    Criação do Projeto Manager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialCustomModule } from 'src/modules/material.module';
+
+import { LoginComponent } from './login/login.component';
+import { SvgModule } from 'src/modules/svg.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialCustomModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SvgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
